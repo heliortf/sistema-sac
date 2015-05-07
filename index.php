54f275cb3bbfba528265b7b4f56bcb11b4928b0a
Ricardo <heliortf@gmail.com>
2015-05-06 23:33:10 -0300

    Layout iniciado e bibliotecas front end adicionadas

--- index.php
+++ index.php
@@ -4,12 +4,22 @@ require_once("vendor/autoload.php");
 require_once("config/autoload.php");
 
 $app = new \Slim\Slim(array(
-    'templates.path' => 'templates'
+    'view' => new \Slim\Views\Twig()
 ));
+
+$view = $app->view();
+$view->parserOptions = array(
+    'debug' => true,
+    'cache' => dirname(__FILE__) . '/cache'
+);
+$view->parserExtensions = array(
+    new \Slim\Views\TwigExtension()
+);
+
 
 // Pagina inicial
 $app->get('/', function() use($app) {
-    $app->render('home/index.php');
+    $app->render('home/index.html.twig');
 })->name("home");
 
 
--- templates/home/index.php
+++
@@ -1,4 +0,0 @@
-<?php require_once('templates/layout/header.php'); ?>
-<h1>Pagina inicial</h1>
-<br/><br/><br/>
-<?php require_once("templates/layout/footer.php"); ?>
