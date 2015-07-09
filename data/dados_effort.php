80d1f2d7feed52568ce5daf8c7d1d0a444e777ef
Ricardo <heliortf@gmail.com>
2015-07-08 23:49:39 -0300

    Alterações na visualização da empresa

--- data/dados_effort.php
+++ data/dados_effort.php
@@ -19,23 +19,11 @@ $E->setAdmin(true);
 $em->persist($E);
 $em->flush();
 
-
-// Cadastra o cargo
-$C = new Cargo();
-$C->setNome(Cargo::ATENDENTE);
-$C->setEmpresa($E);
-
 $C2 = new Cargo();
 $C2->setNome(Cargo::ADMINISTRADOR);
 $C2->setEmpresa($E);
 
-$C3 = new Cargo();
-$C3->setNome(Cargo::RESPONSAVEL_AREA);
-$C3->setEmpresa($E);
-
-$em->persist($C);
 $em->persist($C2);
-$em->persist($C3);
 $em->flush();
 
 // Cadastra a área
@@ -47,47 +35,9 @@ $A2 = new Area();
 $A2->setEmpresa($E);
 $A2->setNome("Diretoria");
 
-$A3 = new Area();
-$A3->setEmpresa($E);
-$A3->setNome("Financeiro");
-
 $em->persist($A);
 $em->persist($A2);
-$em->persist($A3);
 $em->flush();
-
-$U = new Usuario();
-$U->setEmpresa($E);
-$U->setCargo($C);
-$U->setArea($A);
-$U->setCpf(13447868716);
-$U->setNome("Helio Ricardo");
-$U->setEmail("heliortf@gmail.com");
-$U->setLogin("atendente");
-$U->setSenha('12345');
-$U->setDddCelular(21);
-$U->setCelular(992098791);
-$U->setDddTelefone(21);
-$U->setTelefone(27687236);
-
-$em->persist($U);
-
-
-$U2 = new Usuario();
-$U2->setEmpresa($E);
-$U2->setCargo($C3);
-$U2->setArea($A);
-$U2->setCpf(13643828711);
-$U2->setNome("João Lopes");
-$U2->setEmail("joao.lopes@gmail.com");
-$U2->setLogin("responsavel");
-$U2->setSenha('12345');
-$U2->setDddCelular(21);
-$U2->setCelular(992491622);
-$U2->setDddTelefone(21);
-$U2->setTelefone(27685432);
-
-$em->persist($U2);
 
 
 $U3 = new Usuario();
